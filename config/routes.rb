0dfc17c59a528cf431036e3dd4419c1dfdf3123e
出口友梨 <yurinco1119@gmail.com>
2022-10-11 00:07:21 +0900

    記事アプリを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'articles#index'
+
+  resources :articles do
+    resources :comments, only: [:new, :create]
+
+    resource :like, only: [:create, :destroy]
+  end
+
+  resource :profile, only: [:show, :edit, :update]
+  resources :favorites, only: [:index]
 end
